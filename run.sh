8718f631a9ad4c6a2744e9c876f16ac159f82700
André Girol <andregirol@gmail.com>
2019-04-22 15:19:28 -0300

    Moving to functions

--- run.sh
+++ run.sh
@@ -11,43 +11,56 @@ echo -e "${header} Using the ${red}REMOTE${nc} repo ${remote_repo} for testing"
 echo -e "${header} Using the ${yellow}LOCAL${nc} repo ${local_repo} for testing"
 cd $local_repo
 
-# Cleaning up
-echo -e "${header} Deleting everything"
-rm -rf .git
-rm *.txt
+clear_repository(){
+    # Cleaning up
+    echo -e "${header} Deleting everything"
+    rm -rf .git
+    rm *.txt
+}
 
-echo -e "${header} Initializing empty repository"
-git init
+base_init(){
 
-echo -e "${header} Adding remote"
-git remote add origin $remote_repo
+    echo -e "${header} Initializing empty repository"
+    git init
 
-echo -e "${header} Creating first commit"
-echo -e "# Releaser Sample" > README.md
-git add . && git commit -m "First commit - README"
+    echo -e "${header} Adding remote"
+    git remote add origin $remote_repo
 
-echo -e "${header} Preparing branches"
-git branch testing
-git branch beta
-git branch production
+    echo -e "${header} Creating first commit"
+    echo -e "# Releaser Sample" > README.md
+    git add . && git commit -m "First commit - README"
+}
 
-echo -e "${header} Listing Branches"
-git branch
+create_local_branches(){
+    echo -e "${header} Preparing local branches"
 
-echo -e "${header} Creating dummy commits"
+    for branch in ${release_pipeline[*]}
+    do
+        git branch ${branch}
+    done
+}
 
-for step in 1 2 3
-do
-    echo "content ${step}" > "file${step}.txt"
-    git add .
-    git commit -m "Commit ${step}"
-done
 
-echo "${header} Push IT!"
+create_dummy_commits(){
+    echo -e "${header} Creating dummy commits"
 
-for branch in ${release_pipeline[*]}
-do
-    echo -e "${header} Pushing ${red} $branch ${nc}"
-    git push -f origin $branch
-done
+    for step in {1..max_objects}
+    do
+        echo "content ${step}" > "file${step}.txt"
+        git add .
+        git commit -m "Commit ${step}"
+    done
+}
+
+push_all_to_remote(){
+
+    echo "${header} Push IT!"
+
+    for branch in ${release_pipeline[*]}
+    do
+        echo -e "${header} FORCE Pushing origin ${red} $branch ${nc}"
+        git push -f origin $branch
+    done
+}
+
 
