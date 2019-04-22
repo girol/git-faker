65d575546672811b478e721762b0d61296c9c805
André Girol <andregirol@gmail.com>
2019-04-22 17:22:06 -0300

    Add prompt user to git faker

--- bin/fake-committer.sh
+++ bin/fake-committer.sh
@@ -4,9 +4,24 @@ source "$(git --exec-path)/git-sh-setup"
 
 USAGE="INTEGER. INTEGER is the number of fake commits to generate"
 
+_prompt_user(){
+
+    while true; do
+
+        echo "This wil generate AND commit $1 files. Proceed? [y/n] "
+        read choice
+        case $choice in
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
 _fake_commits(){
 
     if [[ -n $1 ]]; then
+        _prompt_user
         for ((step=1 ; step<=$1 ; step++)); do
             echo "content ${step}" > "file${step}.txt"
             git add .
